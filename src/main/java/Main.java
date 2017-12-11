ffab5ad3427190b8ad5e5cfe7a30535983742f98
rash240719 <dyll240719@gmail.com>
2017-12-11 15:50:29 -0600

    1 - Added r language functionalities to menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,33 +5,50 @@ public class Main {
     public static void main(String... args) {
         try {
             JRabbit jRabbit = new JRabbit();
-
             String[] options = {"Send",
                     "Receive",
+                    "Say hello from r script",
+                    "Operate Arithmetics",
                     "Sleep -.-zzz"};
-            int choice = JOptionPane.showOptionDialog(new Frame(),
-                    "What will we do today :D ?",
-                    "JRabbit",
-                    JOptionPane.YES_NO_CANCEL_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    options,
-                    options[2]);
-            switch (choice) {
-                case 0:
-                    System.out.println("You chose " + options[choice]);
-                    jRabbit.openSendDialog();
-                    break;
+            int choice = 0;
+            RGUI rgui = new RGUI();
 
-                case 1:
-                    System.out.println("You chose " + options[choice]);
-                    jRabbit.printReceivedMessages();
-                    break;
+            while (choice != options.length - 1) {
+                choice = JOptionPane.showOptionDialog(new Frame(),
+                        "What will we do today :D ?",
+                        "JRabbit",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        options,
+                        options[options.length - 1]);
 
-                default:
-                    System.out.println("You chose " + options[choice]);
-                    System.out.println("No such option -_-,");
-                    break;
+                switch (choice) {
+                    case 0:
+                        System.out.println("You chose " + options[choice]);
+                        jRabbit.openSendDialog();
+                        break;
+
+                    case 1:
+                        System.out.println("You chose " + options[choice]);
+                        jRabbit.printReceivedMessages();
+                        break;
+
+                    case 2:
+                        System.out.println("You chose " + options[choice]);
+                        rgui.executeScript("Hello", null);
+                        break;
+
+                    case 3:
+                        System.out.println("You chose " + options[choice]);
+                        rgui.openOperationsDialog();
+                        break;
+
+                    default:
+                        System.out.println("You chose " + options[choice]);
+                        System.out.println("Good night");
+                        break;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
