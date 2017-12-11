fce66da0b60cdc2a0af271dfa6d562036577c322
rash240719 <dyll240719@gmail.com>
2017-12-11 15:50:57 -0600

    1 - Added r language script execution

--- src/main/java/R.java
+++ src/main/java/R.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import org.renjin.script.RenjinScriptEngineFactory;
+import org.renjin.sexp.SEXP;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import java.io.FileNotFoundException;
 
 public class R {
+    public SEXP executeScript(String script, Object[] args) {
+        RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
+        ScriptEngine engine = factory.getScriptEngine();
+        String dir = System.getProperty("user.dir") + "/src/main/resources/";
+
+        try {
+            int i = 0;
+
+            for (Object o : args) {
+                engine.put("val" + i, o);
+                i++;
+            }
+
+            return (SEXP) engine.eval(new java.io.FileReader(dir + script + ".r"));
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
