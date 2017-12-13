fac5d3edc62ed84579bfb3d8505af3aa840f83b0
rash240719 <dyll240719@gmail.com>
2017-12-13 14:50:33 -0600

    Added Operation class

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Operation {
+    private char operator;
+    private double x;
+    private double y;
+    private Object result;
+
+    public char getOperator() {
+        return operator;
+    }
+
+    public void setOperator(char operator) {
+        this.operator = operator;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
 }
