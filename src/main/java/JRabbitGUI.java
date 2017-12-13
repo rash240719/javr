af0edade26f341dfac84bfae5afb0518229637e3
rash240719 <dyll240719@gmail.com>
2017-12-13 14:49:03 -0600

    Separated JRabbit Module

--- src/main/java/JRabbitGUI.java
+++ src/main/java/JRabbitGUI.java
@@ -1,12 +1,10 @@
-import com.rabbitmq.client.*;
+import org.renjin.sexp.SEXP;
 
 import javax.swing.*;
 import java.awt.*;
-import java.net.URI;
+import java.util.concurrent.CompletableFuture;
 
 public class JRabbitGUI extends JRabbit {
-    private final static String QUEUE_NAME = "testing";
-
     public void openSendDialog() {
         String message = (String) JOptionPane.showInputDialog(
                 new Frame(),
@@ -23,5 +21,64 @@ public class JRabbitGUI extends JRabbit {
         super.receive((String message) -> {
             System.out.printf(" [x] Received '%s'%n", message);
         });
+    }
+
+    public void openOperationSelectionDialog() {
+        String[] options = {"Addition",
+                "Subtraction",
+                "Product",
+                "Division",
+                "Go back"};
+        int choice = 0;
+
+        while (choice != options.length - 1) {
+            choice = JOptionPane.showOptionDialog(new Frame(),
+                    "What operation will we do today :D ?",
+                    "Operation selection",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    options,
+                    options[options.length - 1]);
+            char operator = ' ';
+
+            switch (choice) {
+                case 0:
+                    System.out.println("You chose " + options[choice]);
+                    operator = '+';
+                    break;
+
+                case 1:
+                    System.out.println("You chose " + options[choice]);
+                    operator = '-';
+                    break;
+
+                case 2:
+                    System.out.println("You chose " + options[choice]);
+                    operator = '*';
+                    break;
+
+                case 3:
+                    System.out.println("You chose " + options[choice]);
+                    operator = '/';
+                    break;
+
+                default:
+                    System.out.println("You chose " + options[choice]);
+                    break;
+            }
+
+            if (choice < options.length - 1) openSendOperationDialog(operator);
+        }
+    }
+
+    public void openSendOperationDialog(char operator) {
+        RGUI rgui = new RGUI();
+        Object[] values = rgui.openBinaryOperationDialog();
+
+        if (values != null) {
+            sendOperation(values, operator);
+            receiveResults();
+        }
     }
 }
