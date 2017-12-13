1e1c04bbb57ef558d5118297f605e9f69ac953ec
rash240719 <dyll240719@gmail.com>
2017-12-13 14:47:46 -0600

    Separated GUI Module

---
+++ src/main/java/RIntermediary.java
@@ -0,0 +1,20 @@
+import org.renjin.script.RenjinScriptEngineFactory;
+import org.renjin.sexp.SEXP;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import java.io.FileNotFoundException;
+
+public class RIntermediate extends R {
+    protected SEXP operate(String operation, Object[] values) {
+        SEXP result = null;
+
+        try {
+            result = (SEXP) super.executeScript(operation, values);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return result;
+        }
+    }
+}
